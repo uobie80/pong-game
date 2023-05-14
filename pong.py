736959ccabcc4fd544ab08ea74edf6533fe8524f
Uchenna Obicheta <uobicheta@hotmail.com>
2023-05-13 20:18:52 -0400

    Added motion to ball.

--- pong.py
+++ pong.py
@@ -2,10 +2,15 @@ import pygame
 
 pygame.init()
 
+# Game clock
+clock = pygame.time.Clock()
+
+# Initialize FRPS (frame rate per second)
+speed = 30
+
 # Setup display
 display_width = 500
 display_height = 300
-
 
 # Set ball size, initial position, and speed
 x = 100
@@ -20,14 +25,27 @@ display = pygame.display.set_mode((display_width, display_height))
 # Set caption
 pygame.display.set_caption("Let's Pong!")
 
-# Set display color
-display.fill((0, 0, 0))
-
-# draw ball
-pygame.draw.circle(display, (255, 255, 255), (x, y), radius)
-
-# Update contents of display window
-pygame.display.update()
 
 while True:
-    stayopen = True
+    # Limit frames per second to speed
+    clock.tick(speed)
+
+    # Set display color
+    display.fill((0, 0, 0))
+
+    # Add motion to ball
+    x += dx
+    y += dy
+
+    # Draw ball
+    pygame.draw.circle(display, (255, 255, 255), (x, y), radius)
+
+   # Check if ball hits right or left boundaries
+    if (x < radius or x > display_width - radius):
+        dx *= -1
+    # Check if ball hits top or bottom boundaries
+    if (y < radius) or (y > display_height - radius):
+        dy *= -1
+
+    # Update contents of display window
+    pygame.display.update()
