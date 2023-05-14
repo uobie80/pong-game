9c8a645140a5a645d839327a5d69bf3ddafbc5d8
Uchenna Obicheta <uobicheta@hotmail.com>
2023-05-13 20:04:28 -0400

    Setup initial parameters for pong ball and display.

--- pong.py
+++ pong.py
@@ -6,4 +6,28 @@ pygame.init()
 display_width = 500
 display_height = 300
 
+
+# Set ball size, initial position, and speed
+x = 100
+y = 100
+radius = 10
+dx = 3
+dy = 3
+
 # Create display
+display = pygame.display.set_mode((display_width, display_height))
+
+# Set caption
+pygame.display.set_caption("Let's Pong!")
+
+# Set display color
+display.fill((0, 0, 0))
+
+# draw ball
+pygame.draw.circle(display, (255, 255, 255), (x, y), radius)
+
+# Update contents of display window
+pygame.display.update()
+
+while True:
+    stayopen = True
