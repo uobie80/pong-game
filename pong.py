fda29afcb04d4c2a6cdb5753d105122c19e4db90
Uchenna Obicheta <uobicheta@hotmail.com>
2023-05-13 20:49:11 -0400

    Added functionality to allow user to quit the game.

--- pong.py
+++ pong.py
@@ -5,14 +5,14 @@ pygame.init()
 # Game clock
 clock = pygame.time.Clock()
 
-# Initialize FRPS (frame rate per second)
+# Initialize FPS (frames per second)
 speed = 30
 
 # Setup display
 display_width = 500
 display_height = 300
 
-# Set ball size, initial position, and speed
+# Set ball initial position, size, and speed
 x = 100
 y = 100
 radius = 10
@@ -25,14 +25,22 @@ display = pygame.display.set_mode((display_width, display_height))
 # Set caption
 pygame.display.set_caption("Let's Pong!")
 
-
+# Game Loop
 while True:
-    # Limit frames per second to speed
+
+    # Limit frames per second to 30
     clock.tick(speed)
+
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
 
     # Set display color
     display.fill((0, 0, 0))
 
+    # Render game
     # Add motion to ball
     x += dx
     y += dy
@@ -40,10 +48,11 @@ while True:
     # Draw ball
     pygame.draw.circle(display, (255, 255, 255), (x, y), radius)
 
-   # Check if ball hits right or left boundaries
+   # Check if ball hits right or left boundaries of display
     if (x < radius or x > display_width - radius):
         dx *= -1
-    # Check if ball hits top or bottom boundaries
+
+    # Check if ball hits top or bottom boundaries of display
     if (y < radius) or (y > display_height - radius):
         dy *= -1
 
