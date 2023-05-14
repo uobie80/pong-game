6a145567deedcc741610545fbf4edbbc3c2f8328
Uchenna Obicheta <uobicheta@hotmail.com>
2023-05-13 22:18:49 -0400

    Added functionality for scorekeeping

--- pong.py
+++ pong.py
@@ -25,6 +25,9 @@ paddle_y = 10
 paddle_width = 3
 paddle_height = 40
 
+# Initialize game score
+game_score = 0
+
 # Create display
 display = pygame.display.set_mode((display_width, display_height))
 
@@ -58,7 +61,9 @@ def hit_paddle():
     """
     Function to detect if ball hit paddle
     """
+    global game_score
     if x - radius <= paddle_x + paddle_width and y > paddle_y and y < paddle_y + paddle_height:
+        game_score += 100
         return True
     return False
 
@@ -67,6 +72,7 @@ def game_over():
     """
     Function to exit out of game if user presses specific keys
     """
+    global game_score
     end_game = True
 
     # Clear screen before displaying text
@@ -99,6 +105,14 @@ def game_over():
         center=(int(display_width/2), int(display_height/1.3)))
     display.blit(rinstructions, rinstructions_rect)
 
+    # Display final game score
+    final_score = "Final score: " + str(game_score)
+    score_announce = font_instructions.render(
+        final_score, True, (255, 255, 255))
+    score_announce_rect = score_announce.get_rect(
+        center=(int(display_width/2), int(display_height/2)))
+    display.blit(score_announce, score_announce_rect)
+
     # Display game over text
     pygame.display.flip()
 
@@ -122,18 +136,26 @@ welcome = welcome_screen.render("Let's Play Pong!", True, (255, 255, 255))
 welcome_rect = welcome.get_rect(
     center=(int(display_width/2), int(display_height/3)))
 
+startmsg = welcome_screen.render(
+    "Hit 'y' to start, or autostart in 10 seconds", True, (255, 255, 255))
+startmsg_rect = startmsg.get_rect(
+    center=(int(display_width/2), int(display_height/4)))
 display.blit(welcome, welcome_rect)
+display.blit(startmsg, startmsg_rect)
 pygame.display.flip()
 
 # Pause for 5 seconds
-pygame.time.set_timer(pygame.USEREVENT, 5000)
+pygame.time.set_timer(pygame.USEREVENT, 10000)
 
-# Catch timer event
+# Catch timer and keydown event
 timer_active = True
 while (timer_active):
     for event in pygame.event.get():
         if event.type == pygame.USEREVENT:
             timer_active = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_y:
+                timer_active = False
 
 
 # Game Loop
